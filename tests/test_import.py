7ad5ad3ff49d2635b5995ed8e1edb212b209fa04
thehappycheese <the.nicholas.archer@gmail.com>
2022-03-07 16:33:54 +0800

    cleanup tests

--- tests/test_import.py
+++ tests/test_import.py
@@ -2,22 +2,3 @@
 
 def test_import():
     import megamerge
-
-def test_do_chonk():
-    import megamerge
-    import numpy as np
-    seg = np.array([
-        [0,1,6],
-        [1,5,8]
-    ]).astype("f8")
-    
-    dat = np.array([
-        [0,3,6],
-        [3,6,9]
-    ]).astype("f8")
-    
-    res = megamerge.do_chonk(
-        seg,
-        dat,
-    )
-    print(res)
